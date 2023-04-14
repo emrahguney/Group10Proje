498a0b4a9a8328041c66264ec41a518f2e388757
RaufZamanli <rzamanli2020ps@gmail.com>
2023-04-14 20:47:18 +0400

    Negative scenarios added

--- src/test/java/StepDefinitions/AttestationsSteps.java
+++ src/test/java/StepDefinitions/AttestationsSteps.java
@@ -5,6 +5,7 @@ import Pages.LeftNav;
 import Utilities.WD;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -22,19 +23,39 @@ public class AttestationsSteps {
     @And("edit item which added in first scenario")
     public void editItemWhichAddedInFirstScenario(DataTable dt) {
 
-    dc.waitUntilRefresh();
-    dc.clickFunction(dc.editButton);
-    wait.until(ExpectedConditions.visibilityOf(dc.nameInput));
-    dc.nameInput.clear();
+        dc.waitUntilRefresh();
+        dc.clickFunction(dc.editButton);
+        wait.until(ExpectedConditions.visibilityOf(dc.nameInput));
+        dc.nameInput.clear();
 
-    List<List<String>> keys = dt.asLists(String.class);
+        List<List<String>> keys = dt.asLists(String.class);
 
         for (int i = 0; i < keys.size(); i++) {
             WebElement element = dc.getWebElement(keys.get(i).get(0));
-            dc.sendKeysFunction(element,keys.get(i).get(1));
+            dc.sendKeysFunction(element, keys.get(i).get(1));
+        }
+    }
+
+        @Then("already exists message should be displayed")
+        public void alreadyExistsMessageShouldBeDisplayed() {
+
+            dc.verifyContainsTextFunction(dc.alreadyExists,"already exists");
+
+        }
+
+        @And("user click on search button")
+        public void userClickOnSearchButton() {
+
+            dc.clickOnSearchButton();
+        }
+
+        @Then("there is no data to display should be displayed")
+        public void thereIsNoDataToDisplayShouldBeDisplayed() {
+
+            dc.waitUntilRefresh();
+            dc.verifyContainsTextFunction(dc.noDataToDisplay,"no data to display");
+
         }
 
 
-
-    }
 }
